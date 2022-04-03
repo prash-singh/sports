a2c08a7f620d41b61e729816c11fcd52a5c717cb
prash-singh <prashantsingh2k2@gmail.com>
2022-04-03 15:25:21 +0530

    team

--- src/Components/CricketComponents/Team.jsx
+++ src/Components/CricketComponents/Team.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Team({ teamPlayers, teamName }) {
   return (
@@ -7,14 +7,14 @@ function Team({ teamPlayers, teamName }) {
       <div className="w-1/6 h-auto border-2 border-blue-500 rounded-md  m-2 bg-white">
         <p className="flex justify-center font-bold">Team: {teamName}</p>
         {teamPlayers.map((player) => (
-          //   <Link to={`/player/${player}`} key={player}>
-          <p
-            key={player}
-            className="flex justify-center text-black font-semibold"
-          >
-            {player}
-          </p>
-          //   </Link>
+          <Link to={`/player/${player}`} key={player}>
+            <p
+              key={player}
+              className="flex justify-center text-black font-semibold"
+            >
+              {player}
+            </p>
+          </Link>
         ))}
       </div>
     </>
